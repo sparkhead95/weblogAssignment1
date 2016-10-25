fbb738b9919562991d6cf98d2f99aafaae2ae77c
sparkhead95 <christianosholland@gmail.com>
2016-10-25 12:51:50 +0100

    All weblog work to date

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  helper_method :is_admin?
+
+  protected
+
+  def is_admin?
+    true
+  end
+  
+end
